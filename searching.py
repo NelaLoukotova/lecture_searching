bb58c72e6550ae205baf2615110ef69ed8f35e9f
Loukotová Nela (253727) <253727@vutbr.cz>
2024-04-08 12:26:21 +0200

    Add function for json

--- searching.py
+++ searching.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 # get current working directory path
 cwd_path = os.getcwd()
@@ -13,9 +14,33 @@ def read_data(file_name, field):
     """
     file_path = os.path.join(cwd_path, file_name)
 
+    # Check if the file exists
+    if not os.path.exists(file_path):
+        print(f"File {file_name} does not exist.")
+        return None
+
+    # Check if the file is a json file
+    if not file_name.endswith('.json'):
+        print(f"File {file_name} is not a JSON file.")
+        return None
+
+    # Check if the field is valid
+    with open("sequential.json", 'r') as f:
+        allowed_fields = json.load(f).keys()
+
+    if field not in allowed_fields:
+        print(f"Field {field} is not allowed.")
+        return None
+
+    # Read the json file and return the specified field
+    with open(file_path, 'r') as f:
+        data = json.load(f)
+        return data[field]
+
 
 def main():
-    pass
+    sequential_data = read_data("sequential.json", "unordered_numbers")
+    print("Sequential Data:", sequential_data)
 
 
 if __name__ == '__main__':
