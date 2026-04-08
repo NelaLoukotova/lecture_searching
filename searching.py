806e0380f516f71745634b90c50d22ea79081ab6
NelaLoukotova <nela.loukotova@email.cz>
2026-04-08 09:05:11 +0200

    nacteni dat ze souboru

--- searching.py
+++ searching.py
@@ -12,10 +12,23 @@ def read_data(file_name, field):
     :return: (list, string),
     """
     file_path = os.path.join(cwd_path, file_name)
+    #csw_path = Path.cwd()
+    import json
+    json_filename = 'sequential.json'
+    json_data = {"unordered_numbers", "ordered_numbers", "dna_sequence"}
+    with open(json_filename, 'r', encoding='utf-8') as file_obj:
+        data = json.load(file_obj)
+        if field not in data:
+            return None
+
+        return data[field]
+        json.dump(json_data, file_obj)
+
 
 
 def main():
-    pass
+        sequential_data = read_data("sequential.json", "unordered_numbers")
+        print(sequential_data)
 
 
 if __name__ == '__main__':
